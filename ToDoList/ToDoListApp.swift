285a6597ff15146f5f0a4b0a323327cc661cfebf
Samantha Gatt <smfgatt@gmail.com>
2024-08-14 12:07:47 -0400

    Initial commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Samantha Gatt on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
